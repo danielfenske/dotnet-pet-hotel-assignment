f7999d440e6c76cbc3f82f8eca0d615b8a64d045
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-30 16:21:26 -0500

    Built PowOwner model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -3,7 +3,20 @@ using System;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace pet_hotel
+namespace pet_hotel.Models
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        // unique id
+        public int id { get; set; }
+        // emailAddress
+        [Required]
+        public string emailAddress { get; set; }
+
+        // name
+        [Required]
+        public string name { get; set; }
+        // petCount
+        // public int petCount { get; set; }
+    }
 }
