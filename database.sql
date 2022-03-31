513f28e50ac9c78af25c3317a6516a10a793e3b5
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-31 13:16:39 -0500

    Modified database.sql file

--- database.sql
+++ database.sql
@@ -1,14 +1,14 @@
-INSERT INTO "PetOwner" ("emailAddress", "name")
+INSERT INTO "PetOwners" ("emailAddress", "name")
 VALUES ('quicklift2022@gmail.com', 'swaggerman'), ('hellohello@gmail.com', 'hihi');
 
-INSERT INTO "PetOwner" ("emailAddress", "name")
+INSERT INTO "PetOwners" ("emailAddress", "name")
 VALUES ('catcrazy@gmail.com', 'catlady124');
 
-INSERT INTO "PetOwner" ("emailAddress", "name")
+INSERT INTO "PetOwners" ("emailAddress", "name")
 VALUES ('theHulk@gmail.com', 'HulkHogan');
 
-INSERT INTO "PetOwner" ("emailAddress", "name")
+INSERT INTO "PetOwners" ("emailAddress", "name")
 VALUES ('2cool2cat@frisky.com', 'friskybusiness');
 
-INSERT INTO "PetOwner" ("emailAddress", "name")
+INSERT INTO "PetOwners" ("emailAddress", "name")
 VALUES('fakeEmail@gmail.com', 'GOAT');
